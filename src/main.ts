eab10301868cdfead8eb0d802bafc8f7246886e2
cadamsmith <cadamsmith.dev@gmail.com>
2022-07-06 01:39:13 -0500

    add random colored hex favicon on load

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import App from './App.svelte';
 
+// choose random favicon
+const faviconFileNames = [
+    'favicon_brick.svg',
+    'favicon_lumber.svg',
+    'favicon_ore.svg',
+    'favicon_grain.svg',
+    'favicon_wool.svg',
+];
+const randomFaviconFileName = faviconFileNames[Math.floor(Math.random() * faviconFileNames.length)];
+const randomFaviconURL = './assets/' + randomFaviconFileName;
+
+// set random favicon
+const faviconLink = document.getElementById('faviconLink');
+faviconLink.setAttribute('href', randomFaviconURL);
+
 const app = new App({
     target: document.body,
 });
